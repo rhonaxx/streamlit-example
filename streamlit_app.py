555548eff5ccd7314c0bbdad483115c2847601f7
rhona <89830761+rhonaxx@users.noreply.github.com>
2023-04-11 22:10:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,23 +26,6 @@ def generate_image(prompt):
     image = Image.open(BytesIO(image_response.content))
     return image
 
-#define list of interesting prompts
-prompts = [
-    "A flying car powered by solar energy",
-    "A tree made of candy",
-    "A city on the moon",
-    "A robot serving coffee to people in a park",
-    "A giant sea monster attacking a ship"
-]
-
-#display list of prompts to user
-st.sidebar.title("Select a prompt")
-selected_prompt = st.sidebar.selectbox("", prompts)
-
-#generate image for selected prompt and display
-st.title(selected_prompt)
-image = generate_image(selected_prompt)
-st.image(image, caption=selected_prompt)
 
 #prompt user for new input
 new_prompt = st.text_input("Enter a prompt:")
