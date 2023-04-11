e2b2614a6639b410bf636efdb72f6b01d0945a7d
rhona <89830761+rhonaxx@users.noreply.github.com>
2023-04-11 22:22:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ def generate_image(prompt):
     image = Image.open(BytesIO(image_response.content))
     return image
 
-st.title "Welcome to Image Visualizer."
+st.title ("Welcome to Image Visualizer.")
          "Describe the image. Example: A flying car powered by solar energy."
 
 #prompt user for new input
